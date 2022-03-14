52962fe9aa276f6285653f7962315c4e15701d3c
Julien Gremillot <julien.gremillot@delia.tech>
2022-03-14 12:48:26 +0100

    Affichage bandeau "du coup"

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 let recognizer;
 
+function showAlert(score) {
+ document.querySelector('#score').textContent = Math.round(score * 100)
+ $("#success-alert").fadeTo(2000, 500).slideUp(500, function() {
+   $("#success-alert").slideUp(500);
+ });
+}
+
 function predictWord() {
  // Array of words that the recognizer is trained to recognize.
  const words = recognizer.wordLabels();
  recognizer.listen(({scores}) => {
    // Turn scores into a list of (score,word) pairs.
    scores = Array.from(scores).map((s, i) => ({score: s, word: words[i]}));
-   console.log(scores)
-   // Find the most probable word.
-   scores.sort((s1, s2) => s2.score - s1.score);
-   document.querySelector('#console').textContent = scores[0].word;
+   showAlert(scores[1].score)
  }, {probabilityThreshold: 0.5});
 }
+
+$("#success-alert").hide();
 
 async function app() {
  recognizer = speechCommands.create(
